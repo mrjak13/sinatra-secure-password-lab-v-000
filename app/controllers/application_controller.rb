12345860de306c30144c20652069fa2c211d0583
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 22:47:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,6 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     ##your code here
     user = User.find_by(username: params[:username])
-    binding.pry
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
