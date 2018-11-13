0b765b57207004ddc022b49e923b81d9e242a107
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 22:40:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,10 +41,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    user = User.find_by(username: params[:username])
-    # binding.pry
-    if user == ""
-      redirect "/login"
+    user = User.find_by(username: params[:username]) 
+    
     elsif user && user.authentication(params[:password])
       session[:user_id] = user.id
       redirect "/account"
